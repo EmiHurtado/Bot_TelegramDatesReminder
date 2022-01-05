757f0b929702fcaa56254119f85b9e3a8f9cadde
EmiHurtado <86236241+EmiHurtado@users.noreply.github.com>
2022-01-05 16:03:06 -0600

    Update config.py

--- settings/config.py
+++ settings/config.py
@@ -7,17 +7,29 @@ Created on Thu Dec 23 12:57:39 2021
 
 import os
 
-ENV = "DEV"#<--- change here DEV or PROD#
+#ENV = "DEV"
+ENV = "PROD"
 
-# keys  
 
-if ENV == "DEV":  
-    telegram_key = "5047789487:AAFrTGoR4auz3rP1tLg34QSmW3_OUiQDGpk"  
-    mongodb_key = "mongodb+srv://emhurtadom:Batman1@telegramdatesreminderbo.wlafu.mongodb.net/TelegramDatesReminderBot?retryWrites=true&w=majority"
-    
-elif ENV == "PROD":   
-    import ast   
-    telegram_key = ast.literal_eval(os.environ["telegram_key"])   
-    mongodb_key = ast.literal_eval(os.environ["mongodb_key"])## server  
-    host = "0.0.0.0"  
-    port = int(os.environ.get("PORT", 5000))
+
+## keys
+if ENV == "DEV":
+	from settings import keys
+	telegram_key = keys.telegram_key
+	mongodb_key = keys.mongodb_key
+
+elif ENV == "PROD":
+	import ast
+	telegram_key = ast.literal_eval(os.environ["telegram_key"])
+	mongodb_key = ast.literal_eval(os.environ["mongodb_key"])
+
+
+
+## server
+host = "0.0.0.0"
+port = int(os.environ.get("PORT", 5000))
+webhook = 'https://bot-date-reminder.herokuapp.com/'
+
+
+## fs
+#root = os.path.dirname(os.path.dirname(__file__)) + "/"
