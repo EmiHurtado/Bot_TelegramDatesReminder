59137a7ef51f2e42c03c0ecef72467acd42b312e
EmiHurtado <86236241+EmiHurtado@users.noreply.github.com>
2022-01-10 14:35:14 -0600

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -191,9 +191,33 @@ def scheduler():
             msg = "Today's events: "+", ".join(res)
             bot.send_message(user, msg)
 
+if config.ENV == "DEV":
+    #bot.infinity_polling(True)  
+    bot.polling()
 
 
-# run
+elif config.ENV == "PROD":
+    import flask
+    import threading
+
+    server = flask.Flask(__name__)
+
+    @server.route('/'+config.telegram_key, methods=['POST'])
+    def getMessage():
+        bot.process_new_updates([telebot.types.Update.de_json(flask.request.stream.read().decode("utf-8"))])
+        return "!", 200
+
+    @server.route("/")
+    def webhook():
+        bot.remove_webhook()
+        bot.set_webhook(url=config.webhook+config.telegram_key)
+        return 'Chat with the Bot  <a href ="https://t.me/DatesReminder1Bot">here</a> \or   Check the project code <a href ="https://github.com/EmiHurtado/Bot_TelegramDatesReminder">here</a>', 200
+
+    if __name__ == "__main__":
+        threading.Thread(target=scheduler).start()
+        server.run(host=config.host, port=config.port)
+
+"""# run
 if config.ENV == "DEV":
     #bot.infinity_polling(True)  
     bot.polling()
@@ -221,3 +245,4 @@ elif config.ENV == "PROD":
         if datetime.datetime.now().strftime("%H:%M") in ["05:00","05:01","06:00","06:01","07:00","07:01"]:
             threading.Thread(target=scheduler).start()
         app.run(host=config.host, port=config.port)
+"""
