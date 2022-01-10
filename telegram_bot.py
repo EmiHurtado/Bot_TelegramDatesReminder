1597ad111ac7b822cf20a62a815c5160e60904b4
EmiHurtado <86236241+EmiHurtado@users.noreply.github.com>
2022-01-10 15:34:50 -0600

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -217,7 +217,8 @@ elif config.ENV == "PROD":
         threading.Thread(target=scheduler).start()
         server.run(host=config.host, port=config.port)
 
-"""# run
+"""
+# run
 if config.ENV == "DEV":
     #bot.infinity_polling(True)  
     bot.polling()
@@ -232,17 +233,17 @@ elif config.ENV == "PROD":
     @app.route('/'+config.telegram_key, methods=['POST'])
     def getMessage():
         bot.process_new_updates([telebot.types.Update.de_json(flask.request.stream.read().decode("utf-8"))])
-        return "!", 200
+    #    return "!", 200
 
     @app.route("/")
-    def webhook():
-        bot.remove_webhook()
-        bot.set_webhook(url=config.webhook+config.telegram_key)
-        return 'Chat with the Bot  <a href ="https://t.me/DatesReminder1Bot">here</a> \or   Check the project code <a href ="https://github.com/EmiHurtado/Bot_TelegramDatesReminder">here</a>', 200
-
-    if __name__ == "__main__":
-        print("---", datetime.datetime.now().strftime("%H:%M"), "---")
-        if datetime.datetime.now().strftime("%H:%M") in ["05:00","05:01","06:00","06:01","07:00","07:01"]:
-            threading.Thread(target=scheduler).start()
-        app.run(host=config.host, port=config.port)
+    #def webhook():
+    #    bot.remove_webhook()
+    #    bot.set_webhook(url=config.webhook+config.telegram_key)
+    #    return 'Chat with the Bot  <a href ="https://t.me/DatesReminder1Bot">here</a> \or   Check the project code <a href ="https://github.com/EmiHurtado/Bot_TelegramDatesReminder">here</a>', 200
+    
+    #if __name__ == "__main__":
+    #    print("---", datetime.datetime.now().strftime("%H:%M"), "---")
+    #    if datetime.datetime.now().strftime("%H:%M") in ["05:00","05:01","06:00","06:01","07:00","07:01"]:
+    #        threading.Thread(target=scheduler).start()
+    #    app.run(host=config.host, port=config.port)
 """
